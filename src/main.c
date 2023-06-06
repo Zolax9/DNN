1f7600f73ecd9c21a5004d827353e4df513ceffa
LATL9 <113098926+LATL9@users.noreply.github.com>
2023-06-06 11:46:42 +0100

    NN: create nn file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+#include "nn.h"
+
 int main()
 {
-    printf("Hello, World!\n");
+    float inp_layer[2] = { 1, -1 };
+    float weights[4] = { 0, 1, 1, 0 };
+    float* out_layer = nn_forward_propagate(&inp_layer, 2, 2, &weights);
+    for (size_t i = 0; i < 2; ++i)
+    {
+        printf("%f\n", out_layer[i]);
+    }
 };
