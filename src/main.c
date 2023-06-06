f16f1917f1c226c846d2528ea4b2bdeb2291b05a
LATL9 <113098926+LATL9@users.noreply.github.com>
2023-06-06 21:49:07 +0100

    generate random weights w/ srand()

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "nn.h"
 
 int main()
 {
+    srand(time(NULL));
+
     layer* inp_layer = layer_create(2);
     inp_layer->nodes[0] = 1;
     inp_layer->nodes[1] = -1;
@@ -11,10 +15,14 @@ int main()
     out_layer->nodes[0] = 1;
     out_layer->nodes[1] = 1;
     float weights[out_layer->size][inp_layer->size];
-    weights[0][0] = 0;
-    weights[0][1] = 0.5;
-    weights[1][0] = 0.2;
-    weights[1][1] = 0;
+    for (size_t i = 0; i < out_layer->size; ++i)
+    {
+        for (size_t j = 0; j < inp_layer->size; ++j)
+        {
+            weights[i][j] = rand() % 2000001 / 1000000.0 - 1;
+            printf("%f\n", weights[i][j]);
+        }
+    }
     nn_forward_propagate(inp_layer, out_layer, weights);
     for (size_t i = 0; i < out_layer->size; ++i)
     {
